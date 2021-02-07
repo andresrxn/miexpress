54263393a66f95645fe87b94fd6a0ac0238bbff9
andresrxn <77864035+andresrxn@users.noreply.github.com>
2021-02-07 15:47:17 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -39,3 +39,117 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
+
+const formulario = document.getElementById('form');
+const inputs = document.querySelectorAll('#form input');
+const msg = document.querySelector('#form textarea');
+
+const expresiones = {
+
+    name: /^[a-zA-ZÀ-ÿ\s]{6,40}$/,
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    phone: /^\d{7,20}$/,
+    msg: /^[\s\S]{10,400}$/,
+}
+
+const campos = {
+    name: false,
+    email: false,
+    phone: false,
+    msg: false,
+}
+
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "name":
+            validarCampo(expresiones.name, e.target, 'name');
+            break;
+        case "email":
+            validarCampo(expresiones.email, e.target, 'email');
+            break;
+        case "phone":
+            validarCampo(expresiones.phone, e.target, 'phone');
+            if (phone.value == "") {
+
+                document.querySelector(`.input-phone .input-error`).style.display = "none"
+            }
+            break;
+        case "msg":
+            validarCampo(expresiones.msg, e.target, 'msg');
+            break;
+    }
+}
+
+const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+
+
+        // document.querySelector(`#input-${campo} i`).classList.add('fa-check-circle');
+        // document.querySelector(`#input-${campo} i`).classList.remove('fa-times-circle');
+        document.querySelector(`.input-${campo} .input-error`).style.display = "none"
+        campos[campo] = true;
+    } else {
+
+        // document.querySelector(`#input-${campo} i`).classList.add('fa-times-circle');
+        // document.querySelector(`#input-${campo} i`).classList.remove('fa-check-circle');
+        document.querySelector(`.input-${campo} .input-error`).style.display = "block"
+        campos[campo] = false;
+    }
+    if (input.value == "") {
+
+        document.querySelector(`.input-${campo} .input-error`).style.display = "none"
+    }
+}
+const validarText = (expresion, msg, campo) => {
+    if (expresion.test(msg.value)) {
+
+
+        // document.querySelector(`#input-${campo} i`).classList.add('fa-check-circle');
+        // document.querySelector(`#input-${campo} i`).classList.remove('fa-times-circle');
+        document.querySelector(`.input-msg .input-error`).style.display = "none"
+        campos[campo] = true;
+    } else {
+
+        // document.querySelector(`#input-${campo} i`).classList.add('fa-times-circle');
+        // document.querySelector(`#input-${campo} i`).classList.remove('fa-check-circle');
+        document.querySelector(`.input-msg .input-error`).style.display = "block"
+        campos[campo] = false;
+    }
+    if (msg.value == "") {
+
+        document.querySelector(`.input-msg .input-error`).style.display = "none"
+    }
+}
+
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+msg.addEventListener('keyup', validarFormulario);
+msg.addEventListener('blur', validarFormulario);
+
+form.addEventListener('keyup', () => {
+    if (campos.name && campos.email && campos.phone && campos.msg || campos.name && campos.email && campos.phone == "" && campos.msg) {
+        document.querySelector(`.input-submit .input-error`).style.display = "none";
+    }
+});
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+
+    if (campos.name && campos.email && campos.phone && campos.msg || campos.name && campos.email && campos.phone == "" && campos.msg) {
+        document.querySelector(`.input-submit .input-error`).style.display = "none"
+
+        form.reset();
+        form.submit();
+
+
+        return;
+    } else {
+        document.querySelector(`.input-submit .input-error`).style.display = "block"
+        return;
+    }
+
+});
